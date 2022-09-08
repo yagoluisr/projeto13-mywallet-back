b7b246bb9e195e39cb2ddf031b33ed3aa71837f9
yagoluis <yagoluisroberto@gmail.com>
2022-09-08 12:40:43 -0300

    Rota /sign-up: criada, recebe dados do front e verifica se há um usuário com o e-mail

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { MongoClient } from 'mongodb';
+import bcrypt from 'bcrypt';
 
 dotenv.config();
 
@@ -10,11 +11,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const mongoClient = new MongoClient(MONGO_URI);
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 
 let db;
 mongoClient.connect().then( () => {
     db = mongoClient.db('mywallet')
+});
+
+
+app.post('/sign-up', async (req, res) => {
+    const user = req.body;
+    console.log(user)
+
+    try {
+        const userDB = await db.collection('users').find({email: user.email}).toArray();
+
+        if (userDB.length !== 0) {
+            return res.status(409).send('Já existe um usuário com esse e-mail');
+        }
+
+        const passwordHash = bcrypt.hashSync(user.password, 10);
+
+        const newUser = await db.collection('users').insertOne({...user, password: passwordHash});
+
+        res.status(201).send(newUser);
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
 
 
